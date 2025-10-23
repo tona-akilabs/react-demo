810cee869ba03edfe8f52c9420ed8eb4af980acb
Tona <tona.chheun@akilabs.co>
2025-10-23 12:53:50 +0700

    add

--- app/counter/counter.tsx
+++ app/counter/counter.tsx
@@ -1,17 +1,28 @@
+import {Activity, useMemo, useState} from 'react'
+import {Welcome} from "~/welcome/welcome";
 
 export function Counter() {
+    const [count, setCount] = useState(0)
 
     const handleClick = () => {
         console.log("Click");
+        setCount(count + 1)
+        //setCount((count) => count + 1)
     }
+
+    const isVisible = useMemo(() => count > 5, [count])
 
     return (
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
                 <div className="max-w-[300px] w-full space-y-6 px-4">
                     <p>Hi, ReactJs</p>
-                    <button onClick={handleClick}>Click Me</button>
+                    <button onClick={handleClick}>Click Me { count }</button>
                 </div>
+                {/*{ isVisible && <Welcome /> }*/}
+                <Activity mode={ isVisible ? 'visible' : 'hidden'  }>
+                    <Welcome />
+                </Activity>
             </div>
         </main>
     );
